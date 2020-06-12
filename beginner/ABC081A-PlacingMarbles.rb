b273b81ae71be9f1e6f092a46f1c8fa69dc61392
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-13 08:51:13 +0900

    別解解説

--- beginner/ABC081A-PlacingMarbles.rb
+++ beginner/ABC081A-PlacingMarbles.rb
@@ -1,3 +1,7 @@
 s1, s2, s3 = gets.chomp.split('').map(&:to_i)
 puts "#{s1 + s2+ s3}"
 #split('')とすれば一文字置きで区切れる
+nums = gets.strip.split('').map(&:to_i)
+puts nums.inject(:+)
+#配列に入れて、中身をinjectで計算している
+#nums.sumでもOK
