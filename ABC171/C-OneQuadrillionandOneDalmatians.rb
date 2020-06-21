a4eaaf943b662b1a9d59912f36195b2713a57b70
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-22 08:45:50 +0900

    解答

--- ABC171/C-OneQuadrillionandOneDalmatians.rb
+++ ABC171/C-OneQuadrillionandOneDalmatians.rb
@@ -1,17 +1,30 @@
+# n = gets.to_i
+
+# def num(n)
+#   b = []
+#   arr2 = []
+  
+#   while (n > 0)
+#     b << n % 26
+#     n /= 26
+#   end
+#   b.each do |b|
+#     arr = ("a".."z").to_a
+#     arr2 << arr[b-1]
+#   end
+#   print arr2.reverse.join
+# end
+
+# num(n)
+
 n = gets.to_i
-
-def num(n)
-  b = []
-  arr2 = []
-  while (n > 0)
-    b << n % 26
-    n /= 26
-  end
-  b.each do |b|
-    arr = ("a".."z").to_a
-    arr2 << arr[b-1]
-  end
-  print arr2.reverse.join
+s = []
+while n > 0
+  n -= 1
+  s.unshift ("a".."z").to_a[n%26]
+  # ("a".."z")を配列とした時、インデックスがゼロからなので、n-=1としてずらしている。
+  # 処理ごとでずらすことに注意
+  n /= 26
 end
-
-num(n)
+puts s*''
+# *はjoinと同じ
