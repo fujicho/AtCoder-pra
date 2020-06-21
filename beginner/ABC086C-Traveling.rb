6305be063bc4b93a577e5bbbe5b5dc4c1ec20c70
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-21 11:21:10 +0900

    自己解説

--- beginner/ABC086C-Traveling.rb
+++ beginner/ABC086C-Traveling.rb
@@ -5,23 +5,34 @@
 n = gets.to_i
 data = n.times.map { gets.strip.split.map(&:to_i) }
 current = [0, 0, 0]
+#初期位置は0,0,0
 
 def is_reachable(current, next_data)
   tc, xc, yc = current
+  # 現在時刻、現在地を代入します。
   tn, xn, yn = next_data
+  # dataの要素を代入している。
   dist = (xc - xn).abs + (yc - yn).abs
   
   return false if dist > tn - tc
-
+  # 到達したい距離>到達時間-現在の時刻ならば到達できないのでfalseを返している。
+  # 最短距離で間に合わない
   (dist - (tn - tc)).even?
+  
 end
 
 data.each do |d|
+  # data一つ一つをeach文で検証していく。
   unless is_reachable(current, d)
-    puts "No"
+    # falseならputs "No"
+    puts 'No'
     exit 0
+    #一つでもNGだったらNoを返して終了する。
   end
   current = d
+  # is_rechableがtrueならdをcurrentに代入
+  # この行を消しても通過可能。どうして解答では記述しているのかは謎。
 end
 
 puts 'Yes'
+# 無事全て通過できたら'Yes'を出力。
