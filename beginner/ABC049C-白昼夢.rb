d6703182693eb0d5bd350b17a201fd0137e9ce84
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-27 18:54:46 +0900

    訂正

--- beginner/ABC049C-白昼夢.rb
+++ beginner/ABC049C-白昼夢.rb
@@ -1,21 +1,21 @@
-# s = gets.strip
+s = gets.strip
 
-# words = "dream", "dreamer", "erase", "eraser"
-# words.sort!.reverse!
-# # dream + erase(r)だと前方からサーチするとプレフィックスになってしまう。
-# # だがeraserからサーチした場合はdreamにかからない。ので、wordsの配列操作でこれを回避することができる。
-# # つまり
-# # words = "eraser", "erase", "dreamer", "dream"
-# # としてもACが出てしまうw
-# words.each do |word|
-#   s.gsub!(word, "")
-# end
+words = "dream", "dreamer", "erase", "eraser"
+words.sort!.reverse!
+# dream + erase(r)だと前方からサーチするとプレフィックスになってしまう。
+# だがeraserからサーチした場合はdreamにかからない。ので、wordsの配列操作でこれを回避することができる。
+# つまり
+# words = "eraser", "erase", "dreamer", "dream"
+# としてもACが出てしまうw
+words.each do |word|
+  s.gsub!(word, "")
+end
 
-# if s == ""
-#   puts "YES"
-# else
-#   puts "NO"
-# end
+if s == ""
+  puts "YES"
+else
+  puts "NO"
+end
 
 #本来は文字列を逆転してサーチするのが正攻法っぽいので別解
 s = gets.strip.reverse!
