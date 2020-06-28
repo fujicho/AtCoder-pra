129d03e48889d2c6956f22e27a3816dcc640758e
fujicho <nnnn.sohei.fujita@gmail.com>
2020-06-28 10:41:20 +0900

    自己解説

--- ABC172/A.rb
+++ ABC172/A.rb
@@ -1,10 +1,14 @@
+# n = gets.to_i
+
+# def calc(n)
+#   a = n
+#   b = n**2
+#   c = n**3
+#   puts a+b+c
+# end
+
+# calc(n)
+
+#　普通にこれでよい
 n = gets.to_i
-
-def calc(n)
-  a = n
-  b = n**2
-  c = n**3
-  puts a+b+c
-end
-
-calc(n)
+puts n + n**2 + n**3
